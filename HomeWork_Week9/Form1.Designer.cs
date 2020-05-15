35019f5a6cb5e44c17faf4a8220cf6a4ae09f3b5
riceblast <15198795025@163.com>
2020-05-15 22:38:11 +0800

    第9周作业：用winform改写爬虫程序

---
+++ HomeWork_Week9/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace HomeWork_Week9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnCrawl = new System.Windows.Forms.Button();
+            this.txtResultURL = new System.Windows.Forms.TextBox();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.lblLimitTimes = new System.Windows.Forms.Label();
+            this.txtLimitTimes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(98, 19);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(522, 28);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnCrawl
+            // 
+            this.btnCrawl.Location = new System.Drawing.Point(632, 22);
+            this.btnCrawl.Name = "btnCrawl";
+            this.btnCrawl.Size = new System.Drawing.Size(156, 42);
+            this.btnCrawl.TabIndex = 1;
+            this.btnCrawl.Text = "Crawl";
+            this.btnCrawl.UseVisualStyleBackColor = true;
+            this.btnCrawl.Click += new System.EventHandler(this.btnCrawl_Click);
+            // 
+            // txtResultURL
+            // 
+            this.txtResultURL.Location = new System.Drawing.Point(67, 87);
+            this.txtResultURL.Multiline = true;
+            this.txtResultURL.Name = "txtResultURL";
+            this.txtResultURL.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultURL.Size = new System.Drawing.Size(670, 351);
+            this.txtResultURL.TabIndex = 2;
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(12, 22);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(80, 18);
+            this.lblURL.TabIndex = 3;
+            this.lblURL.Text = "起始网站";
+            // 
+            // lblLimitTimes
+            // 
+            this.lblLimitTimes.AutoSize = true;
+            this.lblLimitTimes.Location = new System.Drawing.Point(12, 56);
+            this.lblLimitTimes.Name = "lblLimitTimes";
+            this.lblLimitTimes.Size = new System.Drawing.Size(80, 18);
+            this.lblLimitTimes.TabIndex = 4;
+            this.lblLimitTimes.Text = "爬取次数";
+            // 
+            // txtLimitTimes
+            // 
+            this.txtLimitTimes.Location = new System.Drawing.Point(98, 53);
+            this.txtLimitTimes.Name = "txtLimitTimes";
+            this.txtLimitTimes.Size = new System.Drawing.Size(100, 28);
+            this.txtLimitTimes.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLimitTimes);
+            this.Controls.Add(this.lblLimitTimes);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.txtResultURL);
+            this.Controls.Add(this.btnCrawl);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnCrawl;
+        private System.Windows.Forms.TextBox txtResultURL;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label lblLimitTimes;
+        private System.Windows.Forms.TextBox txtLimitTimes;
+    }
+}
+
---
+++ HomeWork_Week9/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork_Week9
+{
+    public partial class Form1 : Form
+    {
+        private string absoluteUrlCheckerPattern;
+        private string toBeFilledUrlCheckerPattern; // 未写http，需要进行填充
+        private Crawler crawler; // 爬取器
+
+        public Form1()
+        {
+            InitializeComponent();
+            absoluteUrlCheckerPattern = @"^(http[s]*)://[ ]*(www|mail).[\S]+.[\S]+";
+            toBeFilledUrlCheckerPattern = @"^(www|mail).[\S]+.[\S]+";
+        }
+
+        // 开始爬取网页
+        private void btnCrawl_Click(object sender, EventArgs e)
+        {
+            // 净空原有记录
+            this.txtResultURL.Clear();
+
+            // 正确性校验
+            string originURL = null;
+
+            // 判断网址是否合法
+            if(Regex.IsMatch(txtURL.Text, absoluteUrlCheckerPattern))
+            {
+                originURL = txtURL.Text;
+            }else if(Regex.IsMatch(txtURL.Text, toBeFilledUrlCheckerPattern))
+            {
+                originURL = "http://" + txtURL.Text;
+            }
+            else
+            {
+                // 非法网址
+                MessageBox.Show("输入的网站非法，请重新输入");
+                txtURL.Clear();
+                return;
+            }
+
+            if (!originURL.Trim().EndsWith("/"))
+            {
+                originURL += "/";
+            }
+
+            // 判断数字是否合法
+            if (!Regex.IsMatch(txtLimitTimes.Text.Trim(), @"^[\d]+$"))
+            {
+                // 如果数字不合法
+                MessageBox.Show("输入的爬取次数不合法，请重新输入");
+                txtLimitTimes.Clear();
+                return;
+            }
+
+            // 如果网址和数字均合法，则进行爬取
+            crawler = new Crawler(originURL, Convert.ToInt32(txtLimitTimes.Text));
+            crawler.downloadSinglePage += this.ShowUrl;
+            crawler.downloadComplete += this.Complete;
+            // 使用线程的原因：
+            // 如果不使用线程，则若爬虫不停止，form就无法显示
+            // 多线程可以实现并发，在爬取的同时可以向用户显示网页
+            new Thread(crawler.DownloadPage).Start();
+        }
+
+        private void ShowSingleUrl(string url)
+        {
+            this.txtResultURL.Text += "\n" + url;
+        }
+
+        // 每次成功爬取到一个网页，就将其显示出来
+        private void ShowUrl(string url)
+        {
+            if (this.txtResultURL.InvokeRequired)
+            {
+                Action<String> action = this.ShowSingleUrl;
+                this.Invoke(action, new object[] { url });
+            }
+            else
+            {
+                this.ShowSingleUrl(url);
+            }
+        }
+
+        // 下载结束，提示用户
+        private void ShowComplete()
+        {
+            this.txtResultURL.Text += "\n\n" + "爬取完成";
+        }
+
+        // 下载结束提示用户
+        private void Complete()
+        {
+            if (this.txtResultURL.InvokeRequired)
+            {
+                Action action = this.ShowComplete;
+                this.Invoke(action, new object[] { });
+            }
+            else
+            {
+                this.ShowComplete();
+            }
+        }
+    }
+}
